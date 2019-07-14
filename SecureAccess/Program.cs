9ade98c100f40ec90f2f36f39e1b7aad87942b6d
Suneet Nangia <suneetnangia@gmail.com>
2019-07-14 22:40:16 +0100

    Changed to look for IotHubConnectionString env variable as well for creating module client from connection string.

--- SecureAccess/Program.cs
+++ SecureAccess/Program.cs
@@ -31,7 +31,7 @@ namespace Azure.Iot.Edge.Modules.SecureAccess
                 var services = new ServiceCollection();
 
                 services.AddTransient<IDeviceHost, PureDeviceHost>(isvc =>
-                                    new PureDeviceHost(new ModuleClientWrapper(Environment.GetEnvironmentVariable("EdgeHubConnectionString")),
+                                    new PureDeviceHost(new ModuleClientWrapper(Environment.GetEnvironmentVariable("EdgeHubConnectionString") == null ? Environment.GetEnvironmentVariable("IotHubConnectionString") : null),
                                     new SecureShell(new DeviceClientWrapper(Environment.GetEnvironmentVariable("deviceConnectionString")),
                                     Environment.GetEnvironmentVariable("targetHost"), targetPort)));
 
